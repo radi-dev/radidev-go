0a4518d120d06f1f9bac2d74b12df3587de0b0b4
Radi <evaradi18@gmail.com>
2025-06-30 04:41:06 +0100

    Refactor HTML structure in handler1 and remove inline styles; enhance layout with CSS classes

--- server.go
+++ server.go
@@ -24,7 +24,7 @@ func handler1(w http.ResponseWriter, r *http.Request) {
 	if owner == "" {
 		owner = "Radi"
 	}
-	fmt.Fprintf(w, `<html>%s<img src='/static/RadiDev_banner.gif'/><h1>Welcome to the website of %s.</h1><br/><hr/><br/><h3>Currently serving from my <b>Raspberry Pi</b> Home Server.</h3><br/><hr/><br/><p>This site is still a work in progress.</p><br/><div style='background-color:#f2f9f2;padding:10px'><hr/><br/>Reach me via email at <a href='mailto:evaristusanarado@gmail.com' target='__blank'>evaristusanarado@gmail.com</a> or via <a href='https://wa.me/2348138686782' target='__blank'>WhatsApp</a><br/><hr/></div><br/>%s<br/><hr/><br/><img style='width:200px;height:200;border-radius:200px' src='https://github.com/radi-dev.png' alt='github profile photo'/><br/><hr/><br/><br/><hr/><br/></html>`, headr, owner, form)
+	fmt.Fprintf(w, `<html>%s<body><img src='/static/RadiDev_banner.gif'/><h1>Welcome to the website of %s.</h1><br/><hr/><br/><h3>Currently serving from my <b>Raspberry Pi</b> Home Server.</h3><br/><hr/><br/><p>This site is still a work in progress.</p><br/><div id=contact><hr/><br/>Reach me via email at <a href='mailto:evaristusanarado@gmail.com' target='__blank'>evaristusanarado@gmail.com</a> or via <a href='https://wa.me/2348138686782' target='__blank'>WhatsApp</a><br/><hr/></div><br/>%s<br/><hr/><br/><img id='pic' src='https://github.com/radi-dev.png' alt='github profile photo'/><br/><hr/><br/><br/><hr/><br/></body></html>`, headr, owner, form)
 }
 func handler2(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
@@ -56,98 +56,6 @@ var headr = `<head>
 <script src='https://unpkg.com/htmx.org@1.9.10'></script>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 <link href="/static/styles.css" rel="stylesheet">
-	<style>
-	body {
-	 font-family: 'Montserrat', sans-serif;
-	 margin: 0;
-	 padding: 0;
-	 width: 100vw;
-	 max-width: 1000px;
-	 margin-left: auto;
-	 margin-right: auto;
-	text-align: center;
-	 padding: 20px;
-	 background: #f9f9f9;
-	 }
-
-	 #form-container {
-		padding: 30px 40px;
-		border-radius: 12px;
-		width: 100%;
-		background-color: #fff;
-		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-		box-sizing: border-box;
-		max-width: 500px;
-		margin: 0 auto;
-		text-align: left;
-	 }
-
-	 h2 {
-		text-align: center;
-		margin-bottom: 20px;
-		color: #333;
-	 }
-
-	 label {
-		display: block;
-		margin-bottom: 6px;
-		font-weight: 600;
-	 }
-
-	 input[type="text"],
-	 input[type="email"],
-	 textarea {
-		width: 100%;
-		padding: 10px 14px;
-		margin-bottom: 16px;
-		border: 1px solid #ccc;
-		border-radius: 6px;
-		font-size: 14px;
-		box-sizing: border-box;
-	 }
-
-	 input[type="submit"] {
-      background-color: #4CAF50;
-      color: white;
-      padding: 12px 18px;
-      border: none;
-      border-radius: 6px;
-      cursor: pointer;
-      width: 100%;
-      font-size: 15px;
-      font-weight: bold;
-      transition: background-color 0.2s ease;
-    }
-
-    input[type="submit"]:hover {
-      background-color: #45a049;
-    }
-
-
-	 textarea {
-		resize: vertical;
-		min-height: 80px;
-	 }
-
-	 img {
-		display: block;
-		margin: 0 auto 20px;
-		max-width: 100%;
-		border-radius: 8px;
-	 }
-
-	 p {
-		font-size: 16px;
-	 }
-
-	 @media (max-width: 900px) {
-	 	body{
-		max-width: 100vw;
-		width: 100vw;}
-		#form-container {
-		max-width: 100vw;
-			width: 100vw;}
-  </style>
   </head>`
 var form = `<div id="form-container">
 	<h2>Send Me a Message</h2>
